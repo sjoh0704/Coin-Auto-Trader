d6cbaee4d0f522d266f954aa67118e44b8c786d3
seung <sjoh0704@daum.net>
2022-01-03 12:07:31 +0900

    add install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime
+sudo apt install python3-pip
+pip3 install pyupbit
+pip3 install schedule
+pip3 install pystan==2.19.1.1
+pip3 install convertdate
+pip3 install fbprophet
